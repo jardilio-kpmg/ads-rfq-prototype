26a9fc07b66b1b1c59f3d25a30e8acccfa91ba7e
Jacob Cornwell <jacobcornwell@gmail.com>
2015-06-23 09:32:46 -0400

    Recalls Search - Recalls Search cleanup and prep for searching for product info by barcode.

--- www/modules/recalls/controllers/SearchCtrl.js
+++ www/modules/recalls/controllers/SearchCtrl.js
@@ -44,6 +44,7 @@ main.controller('SearchCtrl', function (/**ng.$rootScope.Scope*/ $scope, $locati
     self.recalls = null;
     self.selecting = false;
     self.searchResultsFeedback = '';
+    self.emptyResultsMessage = 'No Matches Found!';
 
     self.toggleItemSelection = function (item) {
         item.$selected = !item.$selected;
@@ -77,22 +78,60 @@ main.controller('SearchCtrl', function (/**ng.$rootScope.Scope*/ $scope, $locati
     };
 
     /**
+     * Populates a feedback message.
+     * @param [message] {string} A message to display. When omitted, an empty string will be used.
+     */
+    self.displayFeedback = function(message) {
+        if(!message) {
+            message = '';
+        }
+        self.searchResultsFeedback = message;
+    };
+
+    /**
      * Requests recall results based on the code or search terms provided.
      * @param barcode {string} A UPC barcode string to search for.
      */
     self.performSearchByBarcode = function(barcode) {
-        recallSearchService.getRecallsByBarcode(barcode).success(function (result) {
-            self.searchResultsFeedback = '';
-            self.recalls = result.results;
+        //No need to continue if we don't have a barcode to work with.
+        if(!barcode) {
+            return;
+        }
+
+        recallSearchService.getRecallsByBarcode(barcode).success(function(result) {
+            self.displayFeedback();
+            self.recalls = result.results || [];
         }).error(function(error) {
-            self.searchResultsFeedback = error.error.message;
+            self.displayFeedback(error ? error.error.message : self.emptyResultsMessage);
             self.recalls = [];
 
-            if(error.error.code === 'NOT_FOUND') {
-                //TODO: If no results then call to service that will try and get more product info from the supplied upc and then call performSearchByKeyword
-                self.performSearchByKeyword(['chocolate']);
-            }
+            //TODO: Part of next phase where we use the barcode to get additional product data to search on.
+            /*if(error && error.error.code === 'NOT_FOUND') {
+                self.performSearchByBarcodeData(barcode);
+            }*/
         });
+    };
+
+    /**
+     * Requests recall results based on a barcode's product data.
+     * @param barcode {string} A UPC barcode string to search for.
+     */
+    self.performSearchByBarcodeData = function(barcode) {
+        //No need to continue if we don't have a barcode to work with.
+        if(!barcode) {
+            return;
+        }
+
+        //TODO: Implement when barcode service is ready.
+        /*var barcodeData;
+
+        recallSearchService.getBarcodeData(barcode).success(function(result) {
+            //TODO: compile barcode data and pass to performSearchByKeyword([])
+            self.performSearchByKeyword([]);
+        }).error(function(error) {
+            //No matching results after searching based on barcode product data. Move to manual search method.
+            self.resetSearchResults();
+        });*/
     };
 
     /**
@@ -100,19 +139,37 @@ main.controller('SearchCtrl', function (/**ng.$rootScope.Scope*/ $scope, $locati
      * @param keywords {array} An array of keyword strings to match recall records against.
      */
     self.performSearchByKeyword = function(keywords) {
-        recallSearchService.getRecallsByKeyword(keywords).success(function (result) {
-            self.searchResultsFeedback = 'No Exact Matches Found!';
-            self.recalls = result.results;
+        //No need to continue if we don't have keywords to work with.
+        if(!angular.isArray(keywords)) {
+            return;
+        }
+
+        recallSearchService.getRecallsByKeyword(keywords).success(function(result) {
+            self.displayFeedback();
+            self.recalls = result.results || [];
         }).error(function(error) {
-            self.searchResultsFeedback = error.error.message;
+            self.displayFeedback(error ? error.error.message : self.emptyResultsMessage);
             self.recalls = [];
         });
     };
 
+    /**
+     * Resets the recalls array and clears out any feedback messaging.
+     */
+    self.resetSearchResults = function() {
+        self.displayFeedback();
+        self.recalls = [];
+    };
 
     /**
      * Search for recalls when this view loads based
-     * on the provided search key (code).
+     * on the provided search key (barcode).
      */
-    self.performSearchByBarcode(self.barcode);
+    if(self.barcode) {
+        self.performSearchByBarcode(self.barcode);
+    } else {
+        self.resetSearchResults();
+        self.displayFeedback(self.emptyResultsMessage);
+    }
+
 });
--- www/modules/recalls/services/RecallSearchService.js
+++ www/modules/recalls/services/RecallSearchService.js
@@ -8,7 +8,7 @@ var main = require('../main'),
  * @see http://docs.angularjs.org/api/ngResource.$resource
  * @example
  * function MyCtrl(recallSearchService) {
- *      recallSearchService.getData(input1, input2, {option1: 'option1'})
+ *      recallSearchService.getRecallsByBarcode('111111111111', 0)
  *          .success(function (result) {
  *
  *          })
@@ -22,21 +22,25 @@ main.service('recallSearchService', function (/**kpmgAngular.services.kHttp*/ kH
     var self = this,
         fdaApiBase = 'https://api.fda.gov/food/enforcement.json',
         fdaApiKey = 'CGEoOaTA5x5mmrKoA677SU7hW6tLjR94l33eDGic',
-        resultsLimit = 30,
+        defaultResultsLimit = 30,
         defaultRecallStatus = 'ongoing',
         defaultProductType = 'food';
 
     /**
      * Search for recall records based on a UPC code.
      * @param barcode {string} A UPC barcode string to search for. The string will be matched against code_info.
+     * @param recordToStartWith {number} Of the total number of results, which record should be listed first (e.g. 15 would give you 15 - 45 based on the default result limit).
      * @returns {{success: Function, error: Function}}
      */
-    self.getRecallsByBarcode = function (barcode) {
+    self.getRecallsByBarcode = function (barcode, recordToStartWith) {
+        if(!recordToStartWith) {
+            recordToStartWith = 0;
+        }
         return kHttp.get(fdaApiBase + '?api_key=:apiKey&limit=:limit&skip=:skip&search=status::status+AND+product_type::type+AND+code_info::barcode', {
             params: {
                 apiKey: fdaApiKey,
-                limit: resultsLimit,
-                skip: 0,
+                limit: defaultResultsLimit,
+                skip: recordToStartWith,
                 status: defaultRecallStatus,
                 type: defaultProductType,
                 barcode: barcode
@@ -47,11 +51,15 @@ main.service('recallSearchService', function (/**kpmgAngular.services.kHttp*/ kH
     /**
      * Search for recall records that meet one or more supplied keywords.
      * @param keywords {array} A list of search strings to search by. The keywords will be matched against product_description.
+     * @param recordToStartWith {number} Of the total number of results, which record should be listed first (e.g. 15 would give you 15 - 45 based on the default result limit).
      * @returns {{success: Function, error: Function}}
      */
-    self.getRecallsByKeyword = function (keywords) {
+    self.getRecallsByKeyword = function (keywords, recordToStartWith) {
         var combinedKeywords, escapedKeyword;
 
+        if(!recordToStartWith) {
+            recordToStartWith = 0;
+        }
         //Break apart the array of search keywords into one string.
         //See https://open.fda.gov/api/reference/#query-syntax for more info
         if(keywords && angular.isArray(keywords)) {
@@ -70,12 +78,22 @@ main.service('recallSearchService', function (/**kpmgAngular.services.kHttp*/ kH
         return kHttp.get(fdaApiBase + '?api_key=:apiKey&limit=:limit&skip=:skip&search=status::status+AND+product_type::type' + combinedKeywords, {
             params: {
                 apiKey: fdaApiKey,
-                limit: resultsLimit,
-                skip: 0,
+                limit: defaultResultsLimit,
+                skip: recordToStartWith,
                 status: defaultRecallStatus,
                 type: defaultProductType
             }
         });
+    };
+
+    /**
+     * Gets additional product data bound to the upc barcode.
+     * @param barcode {string} A UPC barcode string to search for. The string will be matched against code_info.
+     * @returns {{success: Function, error: Function}}
+     */
+    self.getBarcodeData = function(barcode) {
+        //TODO: The call to getRecallsByBarcode is temporary. Call new service when it is ready.
+        return self.getRecallsByBarcode(barcode, 0);
     };
 
     /**
@@ -87,7 +105,7 @@ main.service('recallSearchService', function (/**kpmgAngular.services.kHttp*/ kH
         return kHttp.get(fdaApiBase + '?api_key=:apiKey&limit=:limit&skip=:skip&search=status::status+AND+product_type::type&search=recall_number::recallId', {
             params: {
                 apiKey: fdaApiKey,
-                limit: resultsLimit,
+                limit: 1,
                 skip: 0,
                 status: defaultRecallStatus,
                 type: defaultProductType,
